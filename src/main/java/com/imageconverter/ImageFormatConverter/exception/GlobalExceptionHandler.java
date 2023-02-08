1f993f8aed23d9257ff29317682ac733e99e92dd
Jayadev <jayadevplanchuvattil@gmail.com>
2023-02-09 01:15:57 +0530

    updated imports

--- src/main/java/com/imageconverter/ImageFormatConverter/exception/GlobalExceptionHandler.java
+++ src/main/java/com/imageconverter/ImageFormatConverter/exception/GlobalExceptionHandler.java
@@ -2,13 +2,13 @@ package com.imageconverter.ImageFormatConverter.exception;
 
 import com.imageconverter.ImageFormatConverter.model.ErrorObject;
 import com.imageconverter.ImageFormatConverter.model.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import javax.servlet.http.HttpServletRequest;
 
 
 @Slf4j
